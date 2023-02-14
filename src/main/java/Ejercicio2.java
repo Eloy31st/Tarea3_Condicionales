2a259cb8056ec669148293331adddb39692cdd31
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-02-14 16:07:43 +0100

    Ejercicio2

---
+++ src/main/java/Ejercicio2.java
@@ -0,0 +1,49 @@
+public class Ejercicio2 {
+    /*Escriba el nombre del mes completo en función de su número (introducido por el usuario).
+
+     */
+    public static void main(String[] args) {
+        int mes = 1;
+        switch (mes) {
+            case 1:
+                System.out.println("Enero");
+                break;
+            case 2:
+                System.out.println("Febrero");
+                break;
+            case 3:
+                System.out.println("Marzo");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Mayo");
+                break;
+            case 6:
+                System.out.println("Junio");
+                break;
+            case 7:
+                System.out.println("Julio");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Septiembre");
+                break;
+            case 10:
+                System.out.println("Octubre");
+                break;
+            case 11:
+                System.out.println("Noviembre");
+                break;
+            case 12:
+                System.out.println("Diciembre");
+                break;
+            default:
+                System.out.println("Mes no válido");
+                break;
+        }
+    }
+}
