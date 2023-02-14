e696f0a3786964f27244f52a2a474799bebb8cc4
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-02-14 15:46:14 +0100

    Initial commit

---
+++ src/main/java/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+    /* Escriba un algoritmo que muestre "riesgo de hielo", si la temperatura introducida es inferior a 2°C.
+
+     */
+    public static void main(String[] args) {
+        int temperatura = 1;
+        if (temperatura < 2) {
+            System.out.println("Riesgo de hielo");
+        }
+    }
+}
